5750d9e10be17337bb0f1d3739cc62331717c0e0
Roman Valentii <70818538+Roman1917@users.noreply.github.com>
2020-09-26 18:22:19 +0300

    10D

---
+++ Labnumber10E(course2 part 1)/Labnumber10E(course2 part 1)/Labnumber10E(course2 part 1).cpp
@@ -0,0 +1,62 @@
+#include "Labnumer10E.h"
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int n;
+	int m;
+	int si, sj, ix, ij;
+
+	cout << "Введите размер матрицы: " << endl;
+	cin >> n;
+	cin >> m;
+	Matrix a(n, m);
+	for (;;) {
+		int menu;
+		srand(time(NULL));
+
+		cout << "1 Генерация массива 2 Вывести массив 3 Поиск минимального значения" << endl;
+		cout << "4 Получение элемента по позиции и запись 5 Получение подматирцы" << endl;
+		cin >> menu;
+		switch (menu) {
+		case(1):
+			a.randArr();
+			break;
+		case(2):
+			a.showArr();
+			break;
+		case(3):
+			a.findMin();
+			break;
+		case(4):
+			int x;
+			int y;
+
+			cout << "Введите x: ";
+			cin >> x;
+
+			cout << "Введите y: ";
+			cin >> y;
+			a.findElement(x, y);
+			break;
+
+		case(5):
+
+			cout << "С какой позиции : ";
+			cin >> si;
+			cin >> sj;
+			cout << "размер матрицы: ";
+			cin >> ix;
+			cin >> ij;
+			if (si + ix <= n || sj + ij <= m)
+				a.getNewArr(si, sj, ix, ij);
+			else
+				cout << "введите другие значения" << endl;
+			break;
+		default:
+			exit(0);
+
+		}
+	}
+}
